2f6b616501660a5e026034a9ee962bd220ca610e
nathandunn <nathan.dunn.aml@gmail.com>
2023-10-12 12:42:56 -0500

    Changes programs names

--- src/programs/ggw1.ts
+++ src/programs/ggw1.ts
@@ -1,19 +1,9 @@
 import { Program, Exercises } from '../types';
 
-const {
-  SQUAT,
-  BENCH,
-  PRESS,
-  DEADLIFT,
-  SLDL,
-  ROWS,
-  DIPS_OR_PUSHUPS,
-  CHINS_OR_PULLUPS,
-  ARMS,
-} = Exercises;
+const { SQUAT, BENCH, PRESS, DEADLIFT } = Exercises;
 
 const program: Program = {
-  name: 'Garage Gym Warrior I',
+  name: 'Garage Gym Workout I',
   shortName: 'GGW I',
   notes: ['Coming Soon...'],
   maxes: { [SQUAT]: 0, [BENCH]: 0, [DEADLIFT]: 0, [PRESS]: 0 },
